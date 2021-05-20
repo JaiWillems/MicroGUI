8036d0a6e8993dbe581f5ae56d9c2f276bf76312
JaiWillems <jai52h@hotmail.com>
2021-05-20 13:57:33 -0600

    Formatting changes.

--- microgui/flir_camera_control.py
+++ microgui/flir_camera_control.py
@@ -45,23 +45,3 @@ def getTestImage():
     """
     """
     return np.random.randint(0, 255, size=(400, 200, 3)).astype('uint8')
-
-def getTestImageGUI():
-    """
-    """
-    return np.random.randint(0, 255, size=(200, 400, 3)).astype('uint8')
-
-def getDisplay():
-    """
-    """
-    plt.figure(1)
-
-    for i in range(50):
-        index = i/25 * np.pi
-        image_data = np.sin(np.array([np.linspace(index+0, index+2*np.pi, 400) for n in range(0, 200)]))*255
-
-        plt.imshow(image_data)
-
-        plt.pause(0.001)
-
-        plt.clf()
