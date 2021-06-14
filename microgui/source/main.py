a395bfba953cb68032092e93d0fcf2cc80006af3
Jai Willems <jai52h@hotmail.com>
2021-06-14 13:26:09 -0600

    Added safe exit functionality.

--- microgui/source/main.py
+++ microgui/source/main.py
@@ -8,6 +8,7 @@ to run the FAR-IR horizontal microscope.
 from PyQt5.QtWidgets import QApplication
 import sys
 from typing import Dict
+from epics import PV
 
 # Import file dependencies.
 from gui import GUI
@@ -21,10 +22,30 @@ modeMotor = initMotor()
 # Define macro variables.
 data, macros = load_config("config.json")
 
+
+def program_exit(gui: GUI) -> None:
+    """Exit the MicroGUI project.
+
+    This program exits the MicroGUI projects and stops all sample and objective
+    stage motors in the case that the program is closed during operation.
+
+    Parameters
+    ----------
+    gui : GUI
+        Gui containing macro information of the STOP PV names.
+    """
+    app.exec_()
+    for object in ["S", "O"]:
+        for axis in ["X", "Y", "Z"]:
+            pv = PV(gui.macros[f"{axis}{object}STOP"])
+            pv.put(1)
+            pv.put(0)
+
+
 # Run the GUI.
 app = QApplication([])
 app.setStyle("Windows")
 gui = GUI(data=data, macros=macros)
 gui.show()
 Controller(gui=gui, modeMotor=modeMotor)
-sys.exit(app.exec_())
+sys.exit(program_exit(gui))
